57e48dc7fc82d699527d37af70f2245db47c0c9b
Diego Suarez <suarez.pino.diego@gmail.com>
2025-07-07 16:58:24 -0400

    feat port endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import os
 
-
+PORT = os.getenv("PORT", "8000")
 app = FastAPI()
 
 
@@ -13,3 +14,8 @@ def listar_usuarios():
 @app.get("/health")
 def health_check():
     return JSONResponse(content={"status": "ok"}, status_code=200)
+
+
+@app.get("/port")
+async def get_port():
+    return {"port": PORT}
