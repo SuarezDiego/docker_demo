b84b04c8e60cbf535f61fe2f8e99307c03864bb6
Diego Suarez <suarez.pino.diego@gmail.com>
2025-07-08 10:31:25 -0400

    feat stress endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import os
+import time
 
 PORT = os.getenv("PORT", "8000")
 app = FastAPI()
@@ -19,3 +20,13 @@ def health_check():
 @app.get("/port")
 async def get_port():
     return {"port": PORT}
+
+
+@app.get("/stress")
+async def get_stress():
+    end_time = time.time() + 20
+    while time.time() < end_time:
+        x = 0
+        for i in range(10000):
+            x += i ** 2
+    return {"port": f"Stress in port: {PORT}"}
